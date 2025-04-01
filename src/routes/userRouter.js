b7e97fcce52fe61778bd07fe231569b0fa0cb766
Samuel <samuel.thomas1109@outlook.com>
2025-03-31 21:40:40 -0300

    Refactor userValidator path and add validation to edit route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import { showUser, listUsers, createUser, editUser, deleteUser  } from '../controllers/userController.js'
 import validator from "../middlewares/validator.js";
-import schema from "./userValidator.js";
+import schema from "./validators/userValidator.js";
 
 const router = Router()
 
 router.get('/', listUsers)
 router.get('/:_id', showUser)
 router.post('/', validator(schema), createUser)
-router.put('/:_id', editUser)
+router.put('/:_id', validator(schema), editUser)
 router.delete('/:_id', deleteUser)
 
 export default router
